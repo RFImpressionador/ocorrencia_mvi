a3bfafae2d3d531fa3031c70e97b1e331d37fdbd
falconzero7 <rfrodriguesap@gmail.com>
2025-05-30 13:05:06 -0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,9 @@ NUNCA compartilhe essas informações publicamente.
 """
 
 # URL base da API do Supabase
-SUPABASE_URL = "mgowawkzmztwcaxyppum.supabase.co"
+import os
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
 
 # Chave da API (service role ou anon key, dependendo da segurança desejada)
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nb3dhd2t6bXp0d2NheHlwcHVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2MTY4MDAsImV4cCI6MjA2NDE5MjgwMH0.kmdS3dqPWZExI2pjCBqkSCnmvNu8UvGGMrSybJtBv-o"
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
