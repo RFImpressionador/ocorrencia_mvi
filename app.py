b7981650cf392596c09e5c17e35c13fdd4342228
falconzero7 <rfrodriguesap@gmail.com>
2025-06-05 23:25:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from components import form_victims, form_authors, form_witnesses
+from components import form_vitimas, form_autores, form_testemunhas
 from services.listar_ocorrencias import listar_ocorrencias
 from services.supabase_client import save_occurrence
 from models.schema import Ocorrencia
